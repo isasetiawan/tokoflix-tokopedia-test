5d42aef4d52b926cc9c872da7bff96acf52adf0c
Isa Setiawan <azaqokay@gmail.com>
2019-03-22 19:57:12 +0700

    load movies and user

--- src/store.js
+++ src/store.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from './plugins/axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    message:"bgsd"
+    movies:[],
+    user:null
   },
   mutations: {
-
+    setMovies(state, {movies}){
+      state.movies = movies
+    },
+    setUser(state, {user}){
+      state.user = user
+    }
   },
   actions: {
-
+    async loadUser({commit}){
+      let {data} = await axios({baseURL:'https://randomuser.me/api/',url:'?inc=name,picture'}) 
+      commit('setUser',{user:data.results[0]})
+    },
+    async loadMovies({commit}){
+      let {data} = await axios.get('/movie/now_playing')
+      commit('setMovies', {movies:data})
+    }
   }
 })
