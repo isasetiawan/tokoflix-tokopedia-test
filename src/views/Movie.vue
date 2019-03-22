b0e36bb257f4cf597fde51b9e1c767abb2c868e3
Isa Setiawan <azaqokay@gmail.com>
2019-03-23 05:43:45 +0700

    rapi rapi

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -20,7 +20,7 @@
             <div class="columns">
                 <div class="column is-one-fifth">
                    <div class="box is-paddingless">
-                       <figure class="image is-2by3 is-overlay">
+                       <figure class="image is-2by3 ">
                             <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" style="border-radius:6px">
                         </figure>
                    </div>
@@ -63,67 +63,90 @@
                             <a v-if="!movieInCart" class="card-footer-item" @click="purchaseMovie"> Buy for {{moviePrice | currency}}</a>
                             <p v-else class="card-footer-item">Movie Purchased</p>
                         </div>
-                        <!-- {{movie}} -->
-                    </div>
-                </div>
-            </div>
-
-            <div class="columns">
-                <div class="column">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="card-header-title title">Casts</div>
-                        </div>
-                        <div class="card-content">
-                            <div class="columns is-multiline">
-                                <div v-for="cast in casts" :key="cast.id" class="column is-2">
-                                    <div class="box">
-                                        <figure class="image is-2by3">
-                                            <img v-if="cast.profile_path" :src="`http://image.tmdb.org/t/p/w185/${cast.profile_path}`" style="border-radius:6px">
-                                        </figure>
-                                        <b class="is-size-5">{{cast.name}}</b>
-                                        <p>{{cast.character}}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- {{movie}} -->
-        <Loading
-            :is-full-page="true"
-            :active="$store.state.isLoading"
-        />
+        <div class="section">
+            <b-collapse class="card" aria-id="bgsd" :open="false">
+                <div
+                    slot="trigger"
+                    slot-scope="props"
+                    class="card-header"
+                    role="button"
+                    aria-controls="bgsd"
+                >
+                    <p class="card-header-title title">Casts</p>
+                    <a class="card-header-icon">
+                        <b-icon
+                            :icon="props.open ? 'menu-down' : 'menu-up'">
+                        </b-icon>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <div class="content">
+                        <div class="columns is-multiline">
+                            <div v-for="cast in casts" :key="cast.id" class="column is-2">
+                                <div class="box" style="padding:10px">
+                                    <figure class="image" style="width:100px">
+                                        <img v-if="cast.profile_path" :src="`http://image.tmdb.org/t/p/w185/${cast.profile_path}`" style="border-radius:6px">
+                                    </figure>
+                                    <b class="is-size-5">{{cast.name}}</b>
+                                    <p>{{cast.character}}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </b-collapse>
+        </div>
+
+
+        <div class="section">
+            <p class="title">Similar Movies</p>
+            <Movies :movieList="similarMovies"/>
+        </div>
+
+        
     </div>
 </template>
 
 <script>
-import {Loading} from 'buefy/dist/components/loading'
 import {Icon} from 'buefy/dist/components/icon'
-
+import Movies from '@/components/Movies'
 import {mapState, mapActions, mapGetters} from 'vuex'
 export default {
-    components:{Loading, Icon},
+    components:{Icon, Movies,},
     name:'movie',
+    async beforeRouteUpdate(to, _, next){
+        let {movie_id_slug} = to.params
+        let movieId = movie_id_slug.split('-')[0]
+        await this.loadMovie({id:movieId})
+        await this.loadCasts()
+        await this.loadSimilarMovie()
+        next()
+    },
     async created(){
         let {movie_id_slug} = this.$route.params
         let movieId = movie_id_slug.split('-')[0]
         await this.loadMovie({id:movieId})
         await this.loadCasts()
+        await this.loadSimilarMovie()
     },
     methods:{
-        ...mapActions(['loadMovie','loadCasts','purchaseMovie']),
+        ...mapActions([
+            'loadMovie',
+            'loadCasts',
+            'purchaseMovie',
+            'loadSimilarMovie'
+        ]),
     },
-    data(){return{
-        y:0
-    }},
     computed:{
         ...mapState({
             movie: state => state.movie,
-            casts: state => state.casts
+            casts: state => state.casts,
+            similarMovies: state => state.similarMovies
         }),
         ...mapGetters(['moviePrice','movieInCart'])
     },
