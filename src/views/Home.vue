0823b22114718707def74a35c51287d938d9a46e
Isa Setiawan <azaqokay@gmail.com>
2019-03-23 02:38:52 +0700

    display user balance and link to movie detail

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,16 @@
   <div>
 
     <div class="section">
+      <div><b>Balance:</b></div>
+      <div class="is-size-4">{{$store.state.balance | currency}}</div>
+    </div>
+
+    <div class="section">
       <div class="columns is-multiline">
         <div class="column is-one-fifth" v-for="movie in $store.state.movies.results" :key="movie.id">
           <div class="card">
             <div class="card-image">
-              <figure class="image">
+              <figure v-if="movie.poster_path" class="image is-2by3 is-success">
                 <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
               </figure>
             </div>
@@ -15,6 +20,9 @@
             </div>
             <div class="card-content">
               {{movie.overview.slice(0,100)}}...
+            </div>
+            <div class="card-footer">
+              <router-link :to="`/${movie.id}-${movie.title.split(' ').join('-')}`" class="card-footer-item">Buy</router-link>
             </div>
           </div>
         </div>
@@ -27,7 +35,7 @@
 
     <div class="section">
       <Pagination
-            :total="$store.state.movies.total_results"
+            :total="$store.state.movies.total_results-1"
             :current="this.$route.query ? parseInt(this.$route.query.page) : 0"
             order="default"
             :per-page="Math.round($store.state.movies.total_results/$store.state.movies.total_pages)"
@@ -41,9 +49,10 @@
 <script>
 import {Pagination} from 'buefy/dist/components/pagination'
 import {Loading} from 'buefy/dist/components/loading'
+// import {Icon} from 'buefy/dist/components/icon'
 export default {
   name: 'home',
-  components:{Pagination, Loading},
+  components:{Pagination, Loading, },
   async beforeRouteUpdate(to, _, next){
     await this.$store.dispatch('loadMovies',{page:to.query.page})
     next()
