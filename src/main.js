f3d29795d7481d89ffa4005cb7b2a0138cf0d206
Isa Setiawan <azaqokay@gmail.com>
2019-03-23 05:42:50 +0700

    use bebfy globally

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Buefy from 'buefy'
+import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
 import store from './store'
 import currency from './plugins/currency-filter'
 
 Vue.config.productionTip = false
-
+Vue.use(Buefy)
 Vue.filter('currency', currency)
 
 new Vue({
