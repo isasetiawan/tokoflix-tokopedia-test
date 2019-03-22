0d5cf767b1f719af4a7ad64202aefdc6085a7524
Isa Setiawan <azaqokay@gmail.com>
2019-03-22 19:58:03 +0700

    load user and movies before entering home

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import store from './store'
 
 Vue.use(Router)
 
@@ -9,15 +10,13 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Home,
+      async beforeEnter(to, from, next){
+        await store.dispatch('loadUser')
+        await store.dispatch('loadMovies')
+        next()
+      },
+      children:[]
     }
   ]
 })
