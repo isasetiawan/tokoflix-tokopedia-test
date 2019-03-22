92c6e8e0e31430fbd904e14e76076e63b68982db
Isa Setiawan <azaqokay@gmail.com>
2019-03-23 05:43:26 +0700

    use movies components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,7 @@
   <div>
 
     <div class="section">
-      <div class="columns is-multiline">
-        <div class="column is-one-fifth" v-for="movie in $store.state.movies.results" :key="movie.id">
-          <div class="card">
-            <div class="card-image">
-              <figure v-if="movie.poster_path" class="image is-2by3 is-success">
-                <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
-              </figure>
-            </div>
-            <div class="card-header">
-              <div class="card-header-title">{{movie.title}}</div>
-            </div>
-            <div class="card-content">
-              {{movie.overview.slice(0,100)}}...
-            </div>
-            <div class="card-footer">
-              <router-link :to="`/${movie.id}-${movie.title.split(' ').join('-')}`" class="card-footer-item">More info</router-link>
-            </div>
-          </div>
-        </div>
-        <Loading
-          :is-full-page="true"
-          :active="$store.state.isLoading"
-        />
-      </div>
+      <Movies :movieList="$store.state.movies.results"/>      
     </div>
 
     <div class="section">
@@ -43,11 +20,12 @@
 
 <script>
 import {Pagination} from 'buefy/dist/components/pagination'
-import {Loading} from 'buefy/dist/components/loading'
+import Movies from '@/components/Movies'
+
 // import {Icon} from 'buefy/dist/components/icon'
 export default {
   name: 'home',
-  components:{Pagination, Loading, },
+  components:{Pagination, Movies},
   async beforeRouteUpdate(to, _, next){
     await this.$store.dispatch('loadMovies',{page:to.query.page})
     next()
