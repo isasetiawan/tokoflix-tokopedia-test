16f476a66e4b15d1b4a4671c96bb54602219cd8a
Isa Setiawan <azaqokay@gmail.com>
2019-03-22 17:59:50 +0700

    install buefy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import router from './router'
 import store from './store'
 
@@ -8,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  Buefy,
   render: h => h(App)
 }).$mount('#app')
