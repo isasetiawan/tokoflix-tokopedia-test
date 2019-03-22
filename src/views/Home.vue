fed250bd1232b96a4ac88fb8bc65ca108ecacfdd
Isa Setiawan <azaqokay@gmail.com>
2019-03-22 20:41:15 +0700

    display movie list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="section">
+    <div class="columns is-multiline">
+      <div class="column is-one-fifth" v-for="movie in $store.state.movies.results" :key="movie.id">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
+            </figure>
+          </div>
+          <div class="card-header">
+            <div class="card-header-title">{{movie.title}}</div>
+          </div>
+          <div class="card-content">
+            {{movie.overview.slice(0,100)}}...
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
