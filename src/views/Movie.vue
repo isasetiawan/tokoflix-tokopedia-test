3ca866bc9412a0c30bc7d7770f3e4c222a591719
Isa Setiawan <azaqokay@gmail.com>
2019-03-23 04:30:34 +0700

    displaying cast and add ability to purchase movie

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -19,13 +19,11 @@
         <div class="section">
             <div class="columns">
                 <div class="column is-one-fifth">
-                    <div class="card is-success" style="top:-150px; border-radius:10px">
-                        <div class="card-image">
-                            <figure class="image is-2by3 is-overlay">
-                                <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" style="border-radius:10px">
-                            </figure>
-                        </div>
-                    </div>
+                   <div class="box is-paddingless">
+                       <figure class="image is-2by3 is-overlay">
+                            <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" style="border-radius:6px">
+                        </figure>
+                   </div>
                 </div>
 
                 <div class="column">
@@ -49,7 +47,6 @@
                                 />
                                 <span class="is-size-6">{{movie.vote_average}}</span>
                             </div>
-
                             
                             <div class="tags are-medium">
                                 <span class="tag is-primary" v-for="genre in movie.genres" :key="genre.id">{{genre.name}}</span>
@@ -61,7 +58,35 @@
                             </p>
 
                         </div>
+
+                        <div class="card-footer">
+                            <a v-if="!movieInCart" class="card-footer-item" @click="purchaseMovie"> Buy for {{moviePrice | currency}}</a>
+                            <p v-else class="card-footer-item">Movie Purchased</p>
+                        </div>
                         <!-- {{movie}} -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-header-title title">Casts</div>
+                        </div>
+                        <div class="card-content">
+                            <div class="columns is-multiline">
+                                <div v-for="cast in casts" :key="cast.id" class="column is-2">
+                                    <div class="box">
+                                        <figure class="image is-2by3">
+                                            <img v-if="cast.profile_path" :src="`http://image.tmdb.org/t/p/w185/${cast.profile_path}`" style="border-radius:6px">
+                                        </figure>
+                                        <b class="is-size-5">{{cast.name}}</b>
+                                        <p>{{cast.character}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -79,25 +104,28 @@
 import {Loading} from 'buefy/dist/components/loading'
 import {Icon} from 'buefy/dist/components/icon'
 
-import {mapState, mapActions} from 'vuex'
+import {mapState, mapActions, mapGetters} from 'vuex'
 export default {
     components:{Loading, Icon},
     name:'movie',
-    created(){
+    async created(){
         let {movie_id_slug} = this.$route.params
         let movieId = movie_id_slug.split('-')[0]
-        this.loadMovie({id:movieId})
+        await this.loadMovie({id:movieId})
+        await this.loadCasts()
     },
     methods:{
-        ...mapActions(['loadMovie'])
+        ...mapActions(['loadMovie','loadCasts','purchaseMovie']),
     },
     data(){return{
         y:0
     }},
     computed:{
         ...mapState({
-            movie: state => state.movie
-        })
+            movie: state => state.movie,
+            casts: state => state.casts
+        }),
+        ...mapGetters(['moviePrice','movieInCart'])
     },
     
 }
